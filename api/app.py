3324ac3fa044382681047a11f3f0000574e988b3
Aaron Styles <aaron.styles@psma.com.au>
2019-02-01 07:54:27 +1100

    change to the API welcome meassage

--- api/app.py
+++ api/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/api')
 def welcome_api():
-    return "Welcome to the api. THROUGH API!"
+    return "Welcome to the api!"
  
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
