187dbe3eda5f852877c173e2d9c324cc5fd02bc1
Aaron Styles <aaron.styles@psma.com.au>
2019-02-01 08:01:39 +1100

    Added /api/getPoints with static data

--- api/app.py
+++ api/app.py
@@ -1,3 +1,5 @@
+import json
+
 from flask import Flask
  
 app = Flask(__name__)
@@ -5,6 +7,26 @@ app = Flask(__name__)
 @app.route('/api')
 def welcome_api():
     return "Welcome to the api!"
+
+@app.route('/api/getPoints')
+def get_points():
+    sample_geojson_point = {
+      "type": "Feature",
+      "properties": {
+        "dttm": 1548968278,
+        "user": "Aaron",
+        "message": "Single Point Test"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          125.5078125,
+          58.26328705248601
+        ]
+      }
+    }
+
+    return json.dumps(sample_geojson_point)
  
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
